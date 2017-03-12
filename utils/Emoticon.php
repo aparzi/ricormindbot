93d9da4e22676c783fd3e68bdf58b21c823f45ad
Angelo <angeloparziale94@gmail.com>
2017-03-12 15:29:42 +0100

    new emoticons

--- utils/Emoticon.php
+++ utils/Emoticon.php
@@ -10,44 +10,52 @@ class Emoticon {
     static public function smiley() {
         return "\uD83D\uDE03";
     }
-    
+
     static public function joy() {
         return "\uD83D\uDE02";
     }
-    
+
     static public function grin() {
         return "\uD83D\uDE01";
     }
-    
+
     static public function rage() {
         return "\uD83D\uDE21";
     }
-    
+
     static public function check() {
         return "\u2705";
     }
-    
+
     static public function wave() {
         return "\uD83D\uDC4B";
     }
-    
+
     static public function openBook() {
         return "\uD83D\uDCD6";
     }
-    
+
     static public function danger() {
         return "\uD83D\uDEAB";
     }
-    
+
     static public function cross() {
         return "\u274C";
     }
-    
+
     static public function flagUk() {
         return "\uD83C\uDDEC";
     }
-    
+
     static public function flagIt() {
         return "\uD83C\uDDEE";
+    }
+
+    static public function question() {
+      return "\u2753";
+    }
+
+    static public function idea() {
+      return "\uD83D\uDD06";
     }
 }
